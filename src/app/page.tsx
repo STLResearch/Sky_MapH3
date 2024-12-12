9b8732f69ea1edb4c019533a74a19dc50a10ac8e
shantanu kumar kushwaha <121479542+Shan2304@users.noreply.github.com>
2024-12-12 10:44:50 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,24 +2,15 @@
 
 import { useState } from "react";
 import HexMap from "./components/HexMap";
-import HexDetails from "./components/HexDetails";
-
-type HexDetailsType = {
-  hexIndex: string;
-  boundary: number[][];
-  area: number;
-  hexSize: number;
-} | null;
+//import HexDetails from "./components/HexDetails";
 
 export default function Home() {
-  const [hexDetails, setHexDetails] = useState<HexDetailsType>(null);
+  const [ setHexDetails] = useState(null);
 
   return (
     <div className="flex flex-col lg:flex-row w-full h-screen">
       <HexMap setHexDetails={setHexDetails} />
-      <div className="p-4">
-        <HexDetails hex={hexDetails} />
-      </div>
+      
     </div>
   );
 }
